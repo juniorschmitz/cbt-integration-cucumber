a61549905859dc1292c2137347b829fecebed5df
juniorschmitz <juniorschmitz9@gmail.com>
2019-07-27 14:01:22 -0300

    add: first step for the integration

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,33 +1,25 @@
-require "capybara"
-require "capybara/cucumber"
-require "selenium-webdriver"
-require "os"
-require "pry"
+require 'selenium/webdriver'
+require 'capybara/cucumber'
 
-CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_TYPE"]}.yaml"))
+username = "meu_user".sub('@', '%40')
+authkey = "minha_senha"
+url = "http://#{username}:#{authkey}@hub.crossbrowsertesting.com/wd/hub"
 
-case ENV["BROWSER"]
-when "firefox"
-  @driver = :selenium
-when "chrome"
-  @driver = :selenium_chrome
-when "headless"
-  Capybara.register_driver :selenium_chrome_headless do |app|
-    chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
-      opts.add_argument "--headless"
-      opts.add_argument "--disable-gpu"
-      opts.add_argument "--no-sandbox"
-      opts.add_argument "--disable-site-isolation-trials"
-    end
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
-  end
-  @driver = :selenium_chrome_headless
-else
-  puts "Invalid browser"
+#start_tunnel(username,authkey)
+
+Capybara.register_driver 'selenium_remote_cctest'.to_sym do |app|
+  caps = Selenium::WebDriver::Remote::Capabilities.new
+  caps['name'] = 'First FF Test'
+  caps['build'] = '1.0'
+  caps['browserName'] = 'Firefox'
+  caps['platform'] = 'Mac OSX 10.12'
+  caps['screen_resolution'] = '1366x768'
+  caps['record_video'] = 'true'
+  caps['record_network'] = 'false'   
+
+  Capybara::Selenium::Driver.new(app, :browser => :remote, :url => url, :desired_capabilities => caps)
 end
 
-Capybara.configure do |config|
-  config.default_driver = @driver
-  config.app_host = CONFIG["url"]
-  config.default_max_wait_time = 10
-end
+Capybara.default_driver = 'selenium_remote_cctest'.to_sym
+Capybara.app_host = "http://www.google.com"
+Capybara.run_server = false
